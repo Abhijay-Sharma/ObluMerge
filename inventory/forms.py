2b34bc6236f39877ed3e23fcf04f5993d3340668
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-08-18 10:51:24 +0530

    add min requirement and a cron that checks and mails products under min req

--- inventory/forms.py
+++ inventory/forms.py
@@ -18,4 +18,4 @@ class InventoryItemForm(forms.ModelForm):
     category=forms.ModelChoiceField(queryset=Category.objects.all(),initial=0)
     class Meta:
         model=InventoryItem
-        fields=['name','quantity','category']
+        fields=['name','quantity','category','min_quantity']
