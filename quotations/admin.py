62d66574146b8f27c3c30eb2cba8ba7082e18c4e
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-09-03 23:07:19 +0530

    registering new ProductPriceTier in admin.py

--- quotations/admin.py
+++ quotations/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 # Register your models here.
 # quotations/admin.py
 from django.contrib import admin
-from .models import ProductCategory, Product, Quotation, QuotationItem, Customer
+from .models import ProductCategory, Product, Quotation, QuotationItem, Customer, ProductPriceTier
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
 admin.site.register(Quotation)
 admin.site.register(QuotationItem)
 admin.site.register(Customer)
+admin.site.register(ProductPriceTier)
