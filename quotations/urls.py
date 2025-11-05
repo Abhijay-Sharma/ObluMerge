f161960bfaadf902268b5c1cbea40df18907625e
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-11-05 11:05:07 +0530

    reverting urls.py changes

--- quotations/urls.py
+++ quotations/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('new/', views.CreateQuotationView.as_view(), name='create_quotation'),
-    path('<int:pk>/', views.QuotationDetailView.as_view(), name='quotation_detail'),
+    path('<int:pk>/', views.quotation_detail, name='quotation_detail'),
     path('get-customer/', views.get_customer, name='get_customer'),
     path('create-customer/', views.CustomerCreateView.as_view(), name='create_customer'),
     path('customer-list/', views.CustomerListView.as_view(), name='customer_list'),
@@ -14,10 +14,5 @@ urlpatterns = [
     path("products/", views.ProductListView.as_view(), name="product_list") ,
     path("products/<int:pk>/edit/", views.EditProductView.as_view(), name="edit_product"),
     path("products/add/", views.CreateProductView.as_view(), name="create_product"),
-    path("quotation/<int:quotation_id>/request-price-change/", views.PriceChangeRequestCreateView.as_view(),
-         name="request_price_change"),
-    path("price-change-requests/", views.PriceChangeRequestListView.as_view(), name="price_change_requests"),
-    path("price-change-requests/<int:pk>/approve/", views.PriceChangeRequestApproveView.as_view(),
-         name="approve_price_change"),
-    path("price-change-requests/<int:pk>/reject/", views.PriceChangeRequestRejectView.as_view(), name="reject_price_change"),
+
 ]
