0683bf99f26c618873930d3a11cf1502a3023173
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-11-25 11:47:11 +0530

    registered proforma invoice models in template

--- proforma_invoice/admin.py
+++ proforma_invoice/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(ProformaInvoice)
+admin.site.register(ProductPriceTier)
+admin.site.register(ProductPrice)
+admin.site.register(ProformaInvoiceItem)
