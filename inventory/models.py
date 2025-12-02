77e3a0371898208164efed3999591fa10b8b92d7
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-12-02 14:49:50 +0530

    adding expected delivery field in inventory item model

--- inventory/models.py
+++ inventory/models.py
@@ -24,6 +24,7 @@ class InventoryItem(models.Model):
     min_quantity_nitin=models.IntegerField(null=True, blank=True, default=-1)
     unit=models.CharField(max_length=200, blank=True, null=True)
     total_historical_entries=models.IntegerField(null=True, blank=True, default=0)
+    expected_delivery = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
