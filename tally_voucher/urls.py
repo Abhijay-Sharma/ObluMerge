04931d7b65e0001d9390228bbbe55b3d19127bd8
Abhijay Sharma <abhijaysharma0225@gmail.com>
2025-12-03 13:18:12 +0530

    hiding vouchers all from site

--- tally_voucher/urls.py
+++ tally_voucher/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import voucher_list, VoucherDetailView, customer_item_purchases
 
 urlpatterns = [
-    path("all/", voucher_list, name="voucher_list"),
+    # path("all/", voucher_list, name="voucher_list"),
     path("voucher/<int:pk>/", VoucherDetailView.as_view(), name="voucher_detail"),
     path("customer/<int:customer_id>/items/", customer_item_purchases, name="customer_item_purchases"),
 
