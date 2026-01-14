1f75665710595b8603e39b2bab25bb3e04ffe73d
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-01-14 17:41:31 +0530

    registering incentive models in admin

--- incentive_calculator/admin.py
+++ incentive_calculator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ProductIncentive, ProductIncentiveTier
 # Register your models here.
+
+admin.site.register(ProductIncentive)
+admin.site.register(ProductIncentiveTier)
