dfdeec4d91331773755baa7a67ab9d8926b042e3
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-02-02 10:43:40 +0530

    fixing admin.py for customer dashboard

--- customer_dashboard/admin.py
+++ customer_dashboard/admin.py
@@ -26,4 +26,4 @@ class CustomerCreditProfileAdmin(admin.ModelAdmin):
 
 @admin.register(CustomerFollowUp)
 class CustomerFollowUpAdmin(admin.ModelAdmin):
-    list_display = ("salesperson","customer","followup")
+    list_display = ("salesperson","customer","followup_date")
