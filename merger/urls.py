e40a3934bad2bea3d537d02b1ab7b08fe91a47a0
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-02-11 10:49:28 +0530

    Adding Password reset functionality

--- merger/urls.py
+++ merger/urls.py
@@ -18,11 +18,33 @@ from django.contrib import admin
 from django.urls import path , include
 from django.contrib.auth.views import LoginView, LogoutView
 from inventory.views import SignUpView, WelcomeView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',LoginView.as_view() ,name='login'),
     path('logout/',LogoutView.as_view() ,name='logout'),
+    # 🔐 Password reset
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+    ),
     path('inventory/', include('inventory.urls')),
     path('signup/',SignUpView.as_view(),name='signup'),
     path('',WelcomeView.as_view(),name='welcome'),
