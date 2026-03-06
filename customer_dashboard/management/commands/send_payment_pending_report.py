353df64c72b8046bf843a66461927d7c535c71b0
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-03-06 16:30:13 +0530

    pending payment management command email testing

--- customer_dashboard/management/commands/send_payment_pending_report.py
+++ customer_dashboard/management/commands/send_payment_pending_report.py
@@ -141,16 +141,17 @@ class Command(BaseCommand):
                 subject=subject,
                 body="",
                 from_email="crm@oblutools.com",
-                # to=[sp.user.email],
-                to=["madderladder68@gmail.com"],
-                # cc=[
-                #     "abhijay.obluhc@gmail.com",
-                #     "swasti.obluhc@gmail.com",
-                #     "nitin.a@obluhc.com",
-                #     "raman.obluhc@gmail.com",
-                #     "akshay@obluhc.com",
-                #     "bhavya.obluhc@gmail.com",
-                # ],
+                to=[sp.user.email],
+                # to=["madderladder68@gmail.com"],
+                cc=[
+                    "abhijay.obluhc@gmail.com",
+                    "swasti.obluhc@gmail.com",
+                    "nitin.a@obluhc.com",
+                    "raman.obluhc@gmail.com",
+                    "akshay@obluhc.com",
+                    "bhavya.obluhc@gmail.com",
+                    "vibhuti.obluhc@gmail.com"
+                ],
             )
 
             msg.attach_alternative(html_content, "text/html")
