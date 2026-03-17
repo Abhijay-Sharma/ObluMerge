d92406cfa044be0f4c668577c2e7f459d1302469
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-03-17 16:59:09 +0530

    improving tally voucher app admin.py

--- tally_voucher/admin.py
+++ tally_voucher/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Voucher)
-admin.site.register(VoucherRow)
-admin.site.register(VoucherStockItem)
+# admin.site.register(Voucher)
+# admin.site.register(VoucherRow)
+# admin.site.register(VoucherStockItem)
+
+
+@admin.register(Voucher)
+class VoucherAdmin(admin.ModelAdmin):
+    list_display = ('party_name', 'date', 'voucher_type', 'voucher_number','voucher_category')
+    search_fields = ['party_name','voucher_number']
