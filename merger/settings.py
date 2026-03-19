7c9ea3a6ccc3a1b39c4dbffc395da80b2597ebac
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-03-19 20:27:37 +0530

    add the middleware.py file in request logs app. register middleware in settings and make admin.py for request logs

--- merger/settings.py
+++ merger/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'request_logs.middleware.RequestLoggingMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- request_logs/admin.py
+++ request_logs/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import RequestLog
 
-# Register your models here.
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "created_at",
+        "user",
+        "method",
+        "path",
+        "status_code",
+        "execution_time",
+    )
+
+    search_fields = ("path", "user__username")
+
+    list_filter = ("method", "status_code")
