14a1577579d5de581683d816cbd258b7c6b914c0
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-06-01 12:05:29 +0530

    update sync customer voucher command

--- customer_dashboard/management/commands/sync_customer_voucher_status.py
+++ customer_dashboard/management/commands/sync_customer_voucher_status.py
@@ -16,8 +16,8 @@ class Command(BaseCommand):
         total_customers = 0
         total_vouchers_processed = 0
         skipped_no_party_row = 0
-        for customer in Customer.objects.filter(name__icontains="Aline Dent LLP"):
-        # for customer in Customer.objects.all():
+        # for customer in Customer.objects.filter(name__icontains="Aline Dent LLP"):
+        for customer in Customer.objects.all():
             total_customers += 1
 
             try:
