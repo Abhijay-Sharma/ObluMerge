22ce25affd0882de271dfcc090dbc7d4b9b23278
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-08-21 13:04:52 +0530

    incentive calculator updates

--- incentive_calculator/urls.py
+++ incentive_calculator/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('rsm-team-dashboard/', views.RSMTeamIncentiveDashboardView.as_view(), name='rsm_team_dashboard'),
     path('update-customer-trigger/', views.update_customer_trigger, name='update_customer_trigger'),
 
-    path('sales-head-dashboard/', SalesHeadDashboardView.as_view(), name='sales_head_dashboard'),
+    path('sales-head-dashboard/', views.SalesHeadDashboardView.as_view(), name='sales_head_dashboard'),
 ]
