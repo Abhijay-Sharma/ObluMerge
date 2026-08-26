b60577adae1157f92d7f188acffa7c40cb1e8aa1
Abhijay Sharma <abhijaysharma0225@gmail.com>
2026-08-26 12:18:04 +0530

    request log app changes

--- request_logs/urls.py
+++ request_logs/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
-from .views import logs_dashboard
-from .views import log_detail
-from .views import session_timeline
 from . import views
-
 
 urlpatterns = [
     path("", views.LogsDashboardView.as_view(), name="logs_dashboard"),
     path("<int:log_id>/", views.LogDetailView.as_view(), name="log_detail"),
     path("sessions/<str:session_id>/", views.SessionTimelineView.as_view(), name="session_timeline"),
-    path("history/", views.history_view, name="log_history"),
+
+    path("history/", views.HistoryView.as_view(), name="log_history"),
+    path("history/<int:history_id>/", views.HistoryLogDetailView.as_view(), name="history_log_detail"),
+    path("history/sessions/<str:session_id>/", views.SessionTimelineView.as_view(), name="history_session_timeline"),
 ]
